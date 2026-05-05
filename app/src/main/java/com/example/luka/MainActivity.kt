6615655739e8ec316f83739bb9ff9a31ec19ec8b
Jeancarlo Alzate Ramirez <jeancarlo.alzate12@gmail.com>
2026-05-05 18:23:24 -0500

    Mi primer commit

---
+++ app/src/main/java/com/example/luka/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.luka
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.luka.login.LoginScreen
+import com.example.luka.login.LoginViewModel
+import com.example.luka.ui.theme.LUKATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LUKATheme {
+                LoginScreen(LoginViewModel())
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/luka/login/LoginView.kt
@@ -0,0 +1,159 @@
+package com.example.luka.login
+
+import android.provider.ContactsContract
+import com.example.luka.R
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel){
+        Box(Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            ){
+        Login(Modifier.align(Alignment.Center), viewModel)
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier, viewModel: LoginViewModel) {
+
+    val email : String by viewModel.email.observeAsState(initial = "")
+    val password : String by viewModel.password.observeAsState(initial = "")
+    val loginEnable: Boolean by viewModel.loginEnable.observeAsState(initial = false)
+    val isLoading: Boolean by viewModel.isLoading.observeAsState(false)
+    val coroutineScope = rememberCoroutineScope()
+
+    if (isLoading) {
+        Box(Modifier.fillMaxSize()) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    }else{
+        Column(modifier = modifier) {
+            HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = modifier.padding(16.dp))
+            EmailField(email) { viewModel.onLoginChange(it, password) }
+            Spacer(modifier = modifier.padding(4.dp))
+            PasswordField(password) { viewModel.onLoginChange(email, it) }
+            Spacer(modifier = modifier.padding(8.dp))
+            ForgotPassword(Modifier.align(Alignment.End))
+            Spacer(modifier = modifier.padding(16.dp))
+            LoginButton(loginEnable) {
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean, onLoginsSelected:() -> Unit){
+    Button(onClick = { onLoginsSelected() },
+         modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+    colors = ButtonDefaults.buttonColors(
+        containerColor = Color.Red,
+        disabledContainerColor = Color.Blue
+
+    ), enabled = loginEnable
+
+    ) {
+        Text(text = "Sing in",
+            color = Color.White)
+    }
+
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forget Passwordx?",
+        modifier = modifier.clickable{ },
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFFE80A0A)
+        )
+}
+
+@Composable
+fun PasswordField(password : String, onTextFieldChanged: (String)-> Unit){
+    TextField(
+        value = password, onValueChange = {onTextFieldChanged(it)},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text= "Password") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color.Black,
+            unfocusedTextColor = Color.Black,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            focusedPlaceholderColor = Color.LightGray,
+            unfocusedPlaceholderColor = Color.LightGray
+        )
+    )
+}
+@Composable
+
+fun EmailField(email: String, onTextFieldChanged: (String)-> Unit) {
+      TextField(
+        value = email, onValueChange = {onTextFieldChanged(it)},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text= "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+         maxLines = 1,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = Color.Black,
+            unfocusedTextColor = Color.Black,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            focusedPlaceholderColor = Color.LightGray,
+            unfocusedPlaceholderColor = Color.LightGray
+        )
+      )
+    }
+
+@Composable
+fun HeaderImage(modifier: Modifier){
+  Image(
+      painter = painterResource(id = R.drawable.logo),
+      contentDescription = "Header",
+      modifier = modifier
+  )
+}
